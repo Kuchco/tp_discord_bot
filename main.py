785a1980e947709c2e562e803aa9755fbb4088db
Kuchco <adamkuchcik12@gmail.com>
2021-10-19 18:27:57 +0200

    Working member list

--- main.py
+++ main.py
@@ -8,10 +8,9 @@ load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
 
-# intents = discord.Intents().all()
-# client = discord.Client(intents=intents)
-
-client = discord.Client()
+intents = discord.Intents.default()
+intents.members = True
+client = discord.Client(intents=intents)
 
 @client.event
 async def on_ready():

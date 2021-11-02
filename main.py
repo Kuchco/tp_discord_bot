6586556386875c30c3a01d9ebfa2bc5827202ea8
luxao <74666305+luxao@users.noreply.github.com>
2021-11-02 18:14:54 +0100

    study and find some materials for create a roles reactions

--- main.py
+++ main.py
@@ -3,8 +3,10 @@ import os
 
 import discord
 import random
-
+from discord.ext import commands
+import logging
 from dotenv import load_dotenv
+import discord.emoji
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
@@ -13,6 +15,9 @@ GUILD = os.getenv('DISCORD_GUILD')
 intents = discord.Intents.default()
 intents.members = True
 client = discord.Client(intents=intents)
+
+bot = commands.Bot(command_prefix="$",case_insensitive=True)
+logging.basicConfig(level=logging.INFO)
 
 @client.event
 async def on_ready():
@@ -45,9 +50,18 @@ async def on_message(message):
         await message.channel.send(response)
 
     if message.content == "/role":
-        rol = '\n'.join([role.name for role in message.guild.roles])
-        # await message.channel.send(rol)
-        print(rol)
+        # rol = '\n'.join([role.name for role in message.guild.roles[1:]])
+        rol = [role.name for role in message.guild.roles[1:]]
+        for i in range(len(rol)):
+            emoji = '\N{THUMBS UP SIGN}'
+            await message.channel.send(emoji + rol[i] + "\n")
+            print(":kekw: " + rol[i] + "\n")
 
+
+# @bot.command()
+# async def roles(ctx, member):
+#     """Tells you a member's roles."""
+#     member = [role.name for role in member.roles[1:]]
+#     await ctx.send('I see the following roles: ' + ', '.join(member))
 
 client.run(TOKEN)

99b8b28486756c1067f8fc4cd331bc0a27fd58c0
luxao <74666305+luxao@users.noreply.github.com>
2021-12-03 16:08:36 +0100

    change main py and add secret.json

--- main.py
+++ main.py
@@ -1,24 +1,43 @@
-# bot.py
-import os
-
+# Libs
 import discord
-import random
 from discord.ext import commands
 import logging
-from dotenv import load_dotenv
-import discord.emoji
+from pathlib import Path
+import json
 
-load_dotenv()
-TOKEN = os.getenv('DISCORD_TOKEN')
-GUILD = os.getenv('DISCORD_GUILD')
+cwd = Path(__file__).parents[0]
+cwd = str(cwd)
+print(f"{cwd}\n-----")
 
-intents = discord.Intents.default()
-intents.members = True
-client = discord.Client(intents=intents)
-
-bot = commands.Bot(command_prefix="$", case_insensitive=True)
+# create secret file for tokens
+secret = json.load(open(cwd+'/bot_config/secrets.json'))
+bot = commands.Bot(command_prefix='-', case_insensitive=True)
+bot.config_token = secret['DISCORD_TOKEN']
+bot.config_guild = secret['DISCORD_GUILD']
 logging.basicConfig(level=logging.INFO)
 
 
+@bot.event
+async def on_ready():
+    print(f"-----\nLogged in as: {bot.user.name} : {bot.user.id}\n-----\nMy current prefix is: -\n-----")
+    await bot.change_presence(activity=discord.Game(name=f"Ahoj, ja som {bot.user.name}.\na pomôžem vám na serveri!"))
 
-client.run(TOKEN)
+@bot.command(name='hi', aliases=['hello'])
+async def _hi(ctx):
+    """
+    A simple command which says hi to the author.
+    """
+    await ctx.send(f"Hi {ctx.author.mention}!")
+
+
+@bot.command()
+async def echo(ctx, *, message=None):
+    """
+    A simple command that repeats the users input back to them.
+    """
+    message = message or "Prosím zadaj nejakú správu."
+    await ctx.message.delete()
+    await ctx.send(message)
+
+
+bot.run(bot.config_token)

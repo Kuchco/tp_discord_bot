386ae024dc9370d097be3773451c3bbf70408f28
luxao <74666305+luxao@users.noreply.github.com>
2021-12-05 18:51:42 +0100

    create mongo db and create roles reactions

--- main.py
+++ main.py
@@ -1,27 +1,57 @@
 # Libs
-import platform
+
 import os
 import discord
 from discord.ext import commands
 import logging
 from pathlib import Path
 import json
+import motor.motor_asyncio
+from utils.mongo import Document
 
 cwd = Path(__file__).parents[0]
 cwd = str(cwd)
 print(f"{cwd}\n-----")
 
-# create secret file for tokens
+async def get_prefix(bot, message):
+    # If dm's
+    if not message.guild:
+        return commands.when_mentioned_or(bot.default_prefix)(bot, message)
+
+    try:
+        data = await bot.config.find(message.guild.id)
+
+        # Make sure we have a useable prefix
+        if not data or "prefix" not in data:
+            return commands.when_mentioned_or(bot.default_prefix)(bot, message)
+        return commands.when_mentioned_or(data["prefix"])(bot, message)
+    except:
+        return commands.when_mentioned_or(bot.default_prefix)(bot, message)
+
+
+default_prefix = "-"
 secret = json.load(open(cwd+'/bot_config/secret.json'))
-bot = commands.Bot(command_prefix='-', case_insensitive=True)
-bot.config_token = secret['DISCORD_TOKEN']
-bot.config_guild = secret['DISCORD_GUILD']
+
 GUILD = secret['DISCORD_GUILD']
 logging.basicConfig(level=logging.INFO)
 
 intents = discord.Intents.default()
 intents.members = True
 client = discord.Client(intents=intents)
+bot = commands.Bot(
+    command_prefix=get_prefix,
+    case_insensitive=True,
+    owner_id=271612318947868673,
+    help_command=None,
+    intents=intents,
+)
+bot.config_token = secret['DISCORD_TOKEN']
+bot.config_guild = secret['DISCORD_GUILD']
+bot.connection_url = secret["mongo"]
+
+
+bot.default_prefix = default_prefix
+
 
 bot.colors = {
   'WHITE': 0xFFFFFF,
@@ -46,13 +76,20 @@ bot.colors = {
 }
 bot.color_list = [c for c in bot.colors.values()]
 bot.cwd = cwd
-bot.blacklisted_users = []
 
 @bot.event
 async def on_ready():
-
-    print(f"-----\nLogged in as: {bot.user.name} : {bot.user.id}\n-----\nMy current prefix is: -\n-----")
+    # On ready, print some details to standard out
+    print(
+        f"-----\nLogged in as: {bot.user.name} : {bot.user.id}\n-----\nMy current prefix is: {bot.default_prefix}\n-----"
+    )
     await bot.change_presence(activity=discord.Game(name=f"Ahoj, ja som {bot.user.name}.\na pomôžem vám na serveri!"))
+
+    for document in await bot.config.get_all():
+        print(document)
+
+
+    print("Initialized Database\n-----")
 
 
 @bot.event
@@ -61,11 +98,31 @@ async def on_message(message):
     if message.author.id == bot.user.id:
         return
 
+        # Ignore messages sent by yourself
+    if message.author.bot:
+        return
+
+    if message.content.startswith(f"<@!{bot.user.id}>") and len(message.content) == len(
+            f"<@!{bot.user.id}>"
+    ):
+        data = await bot.config.get_by_id(message.guild.id)
+        if not data or "prefix" not in data:
+            prefix = bot.default_prefix
+        else:
+            prefix = data["prefix"]
+        await message.channel.send(f"My prefix here is `{prefix}`", delete_after=15)
+
+
+
     await bot.process_commands(message)
 
 
 
 if __name__ == '__main__':
+    bot.mongo = motor.motor_asyncio.AsyncIOMotorClient(str(bot.connection_url))
+    bot.db = bot.mongo["discordBot"]
+    bot.config = Document(bot.db, "config")
+    bot.reaction_roles = Document(bot.db, "reaction_roles")
     # When running this file, if it is the 'main' file
     # I.E its not being imported from another python file run this
     for file in os.listdir(cwd+"/cogs"):

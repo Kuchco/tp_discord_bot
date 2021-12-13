0dd19e1e52026bb94fccf8e1ef0229e1bb5f16e1
Pavol Švidraň <pavolsvidran@gmail.com>
2021-12-13 09:38:51 +0100

    Implemented archived questions channel

--- cogs/question.py
+++ cogs/question.py
@@ -1,3 +1,4 @@
+import string
 import time
 
 import discord.ext.commands.context
@@ -10,6 +11,13 @@ from discord.ext import commands
 
 class Question(commands.Cog):
     def __init__(self, bot):
+        self.activeChannelName = "active-questions"
+        self.activeChannelTopic = "This channel contains not resolved questions"
+
+        self.archivedChannelName = "archived-questions"
+        self.archivedChannelTopic = "This channel is archive of resolved questions"
+
+        self.channelsCategoryName = "Question-bot channels"
         self.bot = bot
 
     @commands.group(aliases=["q"], invoke_without_command=True, description="Príkaz na položenie otázky")
@@ -20,7 +28,7 @@ class Question(commands.Cog):
     @question.command(aliases=["c"], name="create", description="creates new question, wrap question into \" for question with spaces")
     @commands.guild_only()
     async def question_create(self, context: discord.ext.commands.context.Context, question):
-        channel = await self.__getQuestionChannel(context.guild)
+        channel = await self.__getActiveQuestionsChannel(context.guild)
         question_embed = discord.Embed(
             color=self.bot.colors["BLUE"],
             description=f"{context.author.mention} is asking:\n\n" +
@@ -35,24 +43,48 @@ class Question(commands.Cog):
         await self.bot.question.upsert({"_id": question_message.id, "last_activity": round(time.time())})
         await context.message.delete()
 
-    async def __getQuestionChannel(self, guild: discord.guild):
-        channel_record = await self.bot.guild_question_channel.find_by_id(guild.id)
-        guild_channel = None
+    async def __createCategory(self, guild: discord.guild):
+        return await guild.create_category(self.channelsCategoryName, position=0)
 
-        if channel_record is not None:
-            guild_channel = guild.get_channel(channel_record["channel_id"])
-
-        if guild_channel is None:
-            guild_channel = await self.__createQAndAChannel(guild)
-            await self.bot.guild_question_channel.upsert({"channel_id": guild_channel.id, "_id": guild.id})
-
-        return guild_channel
-
-    async def __createQAndAChannel(self, guild: discord.guild):
-        channel = await guild.create_text_channel("active-questions")
-        await channel.edit(topic="Not resolved questions")
+    async def __createChannel(self, guild, category, name: string, topic: string):
+        channel = await guild.create_text_channel(name)
+        await channel.edit(category=category, topic=topic)
 
         return channel
+
+    async def __createChannels(self, guild: discord.guild):
+        category = await self.__getChannelCategory(guild)
+        active_channel = await self.__createChannel(
+            guild, category, self.activeChannelName, self.activeChannelTopic
+        )
+        archived_channel = await self.__createChannel(
+            guild, category, self.archivedChannelName, self.archivedChannelTopic
+        )
+
+        await self.bot.guild_question_channel.upsert({
+            "active_channel_id": active_channel.id, "archived_channel_id": archived_channel.id, "_id": guild.id
+        })
+
+        return [category, active_channel, archived_channel]
+
+    async def __getChannelCategory(self, guild: discord.guild):
+        for category in guild.categories:
+            if category.name == self.channelsCategoryName:
+                return category
+
+        return await self.__createCategory(guild)
+
+    async def __getActiveQuestionsChannel(self, guild: discord.guild):
+        channel_record = await self.bot.guild_question_channel.find_by_id(guild.id)
+        active_channel = None
+
+        if channel_record is not None:
+            active_channel = guild.get_channel(channel_record["active_channel_id"])
+
+        if active_channel is None:
+            [_, active_channel, _] = await self.__createChannels(guild)
+
+        return active_channel
 
 
 def setup(bot):

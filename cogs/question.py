ed690f43dd9a0ae034fa89ee220bacbb82be6401
Pavol Švidraň <pavolsvidran@gmail.com>
2022-04-07 12:30:04 +0200

    Full code review

--- cogs/question.py
+++ cogs/question.py
@@ -19,7 +19,7 @@ class Question(BaseCommand):
         self.archivedChannelName = "archived-questions"
         self.archivedChannelTopic = "This channel is archive of resolved questions"
 
-        self.errorAnswerWithReactionNotFound = "In thread doesn't exist message marked by you as answer with input emoji"
+        self.errorAnswerWithReactionNotFound = "In thread doesn't exist message emoji-marked by you as answer"
         self.errorUsableOnlyInThread = "Command is usable only inside question thread"
 
         self.channelsCategoryName = "Question-bot channels"
@@ -35,17 +35,17 @@ class Question(BaseCommand):
     @commands.Cog.listener()
     async def on_ready(self):
         await super().on_ready()
-        await self.__start_thread_activity_checker()
+        await self.__startThreadActivityReSpawner()
 
-    @commands.group(aliases=["q"], invoke_without_command=True, description="Group of commands determined for the Q&A system")
     @commands.guild_only()
-    async def question(self, ctx):
-        await ctx.invoke(self.bot.get_command("help"), entity="question")
+    @commands.group(aliases=["q"], invoke_without_command=True, description="Group of commands determined for the Q&A system")
+    async def question(self, context: discord.ext.commands.context.Context) -> None:
+        await context.invoke(self.bot.get_command("help"), entity="question")
 
-    @question.command(aliases=["a"], name="answer", description="Answers (resolves) question, usable only inside active question thread. Wrap answer into \" for question with spaces")
     @commands.guild_only()
     @commands.has_guild_permissions(administrator=True)
-    async def question_answer(self, context: discord.ext.commands.context.Context, answer):
+    @question.command(aliases=["a"], name="answer", description="Answers (resolves) question, usable only inside active question thread. Wrap answer into \" for question with spaces")
+    async def question_answer(self, context: discord.ext.commands.context.Context, answer) -> None:
         question = await self.bot.question.find_by_id(context.channel.id)
         if question is not None:
             await (await self.__getArchivedQuestionsChannel(context.guild)).send(
@@ -55,10 +55,10 @@ class Question(BaseCommand):
         else:
             await self.__sendErrorMessage(context, self.errorUsableOnlyInThread)
 
-    @question.command(aliases=["aem"], name="answer-emoji-marked", description="Answers (resolves) question with specific emoji marked messages. Usable only inside active question thread.")
     @commands.guild_only()
     @commands.has_guild_permissions(administrator=True)
-    async def question_answer_emoji_marked(self, context: discord.ext.commands.context.Context, emoji):
+    @question.command(aliases=["aem"], name="answer-emoji-marked", description="Answers (resolves) question with specific emoji marked messages. Usable only inside active question thread.")
+    async def question_answer_emoji_marked(self, context: discord.ext.commands.context.Context, emoji) -> None:
         question = await self.bot.question.find_by_id(context.channel.id)
         if question is not None:
             answers = []
@@ -66,7 +66,7 @@ class Question(BaseCommand):
                 if self.bot.user.id == message.author.id:
                     continue
 
-                if await self.__is_message_marked_by_user_with_emoji(message, context.author.id, emoji):
+                if await self.__isMessageMarkedByUserWithEmoji(message, context.author.id, emoji):
                     answers.append(message.content)
 
             if len(answers) > 0:
@@ -79,9 +79,9 @@ class Question(BaseCommand):
         else:
             await self.__sendErrorMessage(context, self.errorUsableOnlyInThread)
 
-    @question.command(aliases=["c"], name="create", description="Creates new question, wrap question into \" for question with spaces")
     @commands.guild_only()
-    async def question_create(self, context: discord.ext.commands.context.Context, question):
+    @question.command(aliases=["c"], name="create", description="Creates new question, wrap question into \" for question with spaces")
+    async def question_create(self, context: discord.ext.commands.context.Context, question) -> None:
         channel = await self.__getActiveQuestionsChannel(context.guild)
         question_embed = discord.Embed(
             color=self.bot.colors["BLUE"],
@@ -104,17 +104,96 @@ class Question(BaseCommand):
 
         await context.message.delete()
 
-    @question.command(aliases=["r", "delete", "d"], name="remove", description="Removes question, usable only inside active question thread.")
     @commands.guild_only()
     @commands.has_guild_permissions(administrator=True)
-    async def question_remove(self, context: discord.ext.commands.context.Context):
+    @question.command(aliases=["r", "delete", "d"], name="remove", description="Removes question, usable only inside active question thread.")
+    async def question_remove(self, context: discord.ext.commands.context.Context) -> None:
         question = await self.bot.question.find_by_id(context.channel.id)
         if question is not None:
             await self.__removeQuestion(context, question["_id"])
         else:
             await self.__sendErrorMessage(context, self.errorUsableOnlyInThread)
 
-    async def __check_threads_activity(self):
+    async def __createCategory(self, guild: discord.guild):
+        return await guild.create_category(self.channelsCategoryName, position=0)
+
+    async def __createChannel(self, guild: discord.guild, category: discord.CategoryChannel, name: string, role: discord.Role, topic: string) -> TextChannel:
+        channel = await guild.create_text_channel(name, category=category, topic=topic)
+
+        await channel.set_permissions(role, administrator=True)
+        await channel.set_permissions(
+            guild.default_role,
+            administrator=False,
+            manage_channels=False,
+            manage_permissions=False,
+            manage_messages=False,
+            read_messages=True,
+            send_messages=False
+        )
+
+        return channel
+
+    async def __getActiveQuestionsChannel(self, guild: discord.guild) -> TextChannel:
+        return await self.__getQuestionChannel(guild, "active_channel_id")
+
+    async def __getArchivedQuestionsChannel(self, guild: discord.guild) -> TextChannel:
+        return await self.__getQuestionChannel(guild, "archived_channel_id")
+
+    async def __getChannelCategory(self, guild: discord.guild) -> discord.CategoryChannel:
+        for category in guild.categories:
+            if category.name == self.channelsCategoryName:
+                return category
+
+        return await self.__createCategory(guild)
+
+    async def __getQuestionAnswerEmbed(self, context: discord.ext.commands.context.Context, question_id: int, answers: []) -> discord.Embed:
+        # TODO change - for layout "-"
+        # TODO underline first line with author, every answer should be on own line, include one line ones
+        merged_answer = ""
+        for answer in answers:
+            if len(answers) > 1:
+                merged_answer += "\n - "
+            merged_answer += answer.capitalize()
+
+        raw_question_message = (await (await self.__getActiveQuestionsChannel(context.guild)).fetch_message(question_id)).embeds[0].description
+        message_start = raw_question_message.find(self.question_prefix) + len(self.question_prefix)
+        message_end = raw_question_message.find(self.question_postfix)
+
+        return discord.Embed(
+            color=self.bot.colors["GREEN"],
+            description=f"{context.author.mention} solved question with answer: ***" + merged_answer + "***\n\n",
+            title=raw_question_message[message_start:message_end],
+        )
+
+    async def __getQuestionChannel(self, guild: discord.guild, channel_id: string) -> discord.TextChannel:
+        channel_record = await self.bot.guild_question_channel.find_by_id(guild.id)
+        channel = None
+
+        if channel_record is not None:
+            channel = guild.get_channel(channel_record[channel_id])
+
+        if channel is None:
+            [_, channels] = await self.__setup(guild)
+            channel = channels[channel_id]
+
+        return channel
+
+    async def __isMessageMarkedByUserWithEmoji(self, message: discord.Message, user_id: int, emoji: string) -> bool:
+        answer_reaction = discord.utils.get(message.reactions, emoji=emoji)
+
+        if answer_reaction is not None:
+            async for user in answer_reaction.users():
+                if user.id == user_id:
+                    return True
+
+        return False
+
+    async def __removeQuestion(self, context: discord.ext.commands.context.Context, question_id: int) -> None:
+        await context.channel.parent.get_partial_message(question_id).delete()
+        await context.channel.delete()
+        await self.bot.question.delete(context.channel.id)
+
+    async def respawnQuestionThreadsActivity(self) -> None:
         for question in await self.bot.question.get_all():
             if question["last_activity"] + self.inactiveThreadDurationInS - self.inactiveThreadReminderBeforeExpirationInS > round(time.time()):
                 continue
@@ -155,99 +234,15 @@ class Question(BaseCommand):
                 utils.util.log_error("Question exception: Removing " + format(question["_id"]) + " - no longer exist!")
                 await self.bot.question.delete(question["_id"])
 
-    async def __createCategory(self, guild: discord.guild):
-        return await guild.create_category(self.channelsCategoryName, position=0)
-
-    async def __createChannel(self, guild, category, name: string, question_manager_role, topic: string):
-        channel = await guild.create_text_channel(name, category=category, topic=topic)
-
-        await channel.set_permissions(question_manager_role, administrator=True)
-        await channel.set_permissions(
-            guild.default_role,
-            administrator=False,
-            manage_channels=False,
-            manage_permissions=False,
-            manage_messages=False,
-            read_messages=True,
-            send_messages=False
-        )
-
-        return channel
-
-    async def __getActiveQuestionsChannel(self, guild: discord.guild) -> TextChannel:
-        return await self.__getQuestionChannel(guild, "active_channel_id")
-
-    async def __getArchivedQuestionsChannel(self, guild: discord.guild) -> TextChannel:
-        return await self.__getQuestionChannel(guild, "archived_channel_id")
-
-    async def __getChannelCategory(self, guild: discord.guild):
-        for category in guild.categories:
-            if category.name == self.channelsCategoryName:
-                return category
-
-        return await self.__createCategory(guild)
-
-    async def __getQuestionAnswerEmbed(self, context: discord.ext.commands.context.Context, question_id: int, answers: []) -> discord.Embed:
-        merged_answer = ""
-        for answer in answers:
-            if len(answers) > 1:
-                merged_answer += "\n - "
-            merged_answer += answer.capitalize()
-
-        raw_question_message = (await (await self.__getActiveQuestionsChannel(context.guild)).fetch_message(question_id)).embeds[0].description
-        message_start = raw_question_message.find(self.question_prefix) + len(self.question_prefix)
-        message_end = raw_question_message.find(self.question_postfix)
-
-        return discord.Embed(
-            color=self.bot.colors["GREEN"],
-            description=f"{context.author.mention} solved question with answer: ***" + merged_answer + "***\n\n",
-            title=raw_question_message[message_start:message_end],
-        )
-
-    async def __getQuestionChannel(self, guild: discord.guild, channel_id: string):
-        channel_record = await self.bot.guild_question_channel.find_by_id(guild.id)
-        channel = None
-
-        if channel_record is not None:
-            channel = guild.get_channel(channel_record[channel_id])
-
-        if channel is None:
-            [_, channels] = await self.__setup(guild)
-            channel = channels[channel_id]
-
-        return channel
-
-    async def __is_message_marked_by_user_with_emoji(self, message: discord.Message, user_id: int, emoji) -> bool:
-        answer_reaction = discord.utils.get(message.reactions, emoji=emoji)
-
-        if answer_reaction is not None:
-            async for user in answer_reaction.users():
-                if user.id == user_id:
-                    return True
-
-        return False
-
-    async def __removeQuestion(self, context: discord.ext.commands.context.Context, question_id: int):
-        await context.channel.parent.get_partial_message(question_id).delete()
-        await context.channel.delete()
-        await self.bot.question.delete(context.channel.id)
-
-    async def __sendErrorMessage(self, context: discord.ext.commands.context.Context, text: string):
-        await context.channel.send(embed=discord.Embed(
-            color=self.bot.colors["RED"],
-            title="Usable only inside question thread"
-        ))
+    async def __sendErrorMessage(self, context: discord.ext.commands.context.Context, text: string) -> None:
+        await context.channel.send(embed=discord.Embed(color=self.bot.colors["RED"], title=text))
 
     async def __setup(self, guild: discord.guild):
         category = await self.__getChannelCategory(guild)
-        question_manager_role = await self.__setupRole(guild)
+        role = await self.__setupRole(guild)
 
-        active_channel = await self.__createChannel(
-            guild, category, self.activeChannelName, question_manager_role, self.activeChannelTopic
-        )
-        archived_channel = await self.__createChannel(
-            guild, category, self.archivedChannelName, question_manager_role, self.archivedChannelTopic
-        )
+        active_channel = await self.__createChannel(guild, category, self.activeChannelName, role, self.activeChannelTopic)
+        archived_channel = await self.__createChannel(guild, category, self.archivedChannelName, role, self.archivedChannelTopic)
 
         await self.bot.guild_question_channel.upsert({
             "active_channel_id": active_channel.id, "archived_channel_id": archived_channel.id, "_id": guild.id
@@ -255,7 +250,7 @@ class Question(BaseCommand):
 
         return [category, {"active_channel_id": active_channel, "archived_channel_id": archived_channel}]
 
-    async def __setupRole(self, guild: discord.guild):
+    async def __setupRole(self, guild: discord.guild) -> discord.Role:
         question_manager_role = None
         for role in guild.roles:
             if role.name == self.channelsRoleName:
@@ -274,11 +269,11 @@ class Question(BaseCommand):
 
         return question_manager_role
 
-    async def __start_thread_activity_checker(self):
+    async def __startThreadActivityReSpawner(self) -> None:
         while True:
-            await self.__check_threads_activity()
+            await self.respawnQuestionThreadsActivity()
             await asyncio.sleep(self.inactiveThreadCheckEveryInS)
 
 
-def setup(bot):
+def setup(bot) -> None:
     bot.add_cog(Question(bot))

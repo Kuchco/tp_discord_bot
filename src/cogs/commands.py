7210ad9c4944d4c72e8a55b001cb05bb5c8fe99c
Luxo <74666305+luxao@users.noreply.github.com>
2022-05-09 20:50:25 +0200

    change sk lan to en

--- src/cogs/commands.py
+++ src/cogs/commands.py
@@ -18,13 +18,13 @@ class Commands(BaseCommand):
         server_count = len(self.bot.guilds)
         member_count = len(set(self.bot.get_all_members()))
 
-        embed = discord.Embed(title=f'{self.bot.user.name} - Pomáham moderovať školské discord serveri :angel:', description='\uFEFF', colour=ctx.author.colour, timestamp=ctx.message.created_at)
+        embed = discord.Embed(title=f'{self.bot.user.name} - I help moderate school discord servers :angel:', description='\uFEFF', colour=ctx.author.colour, timestamp=ctx.message.created_at)
         embed.add_field(name='Python Version:', value=version_of_python)
         embed.add_field(name='Discord.Py Version', value=dpy_version)
         embed.add_field(name='Total Guilds:', value=str(server_count))
         embed.add_field(name='Total Users:', value=str(member_count))
 
-        embed.set_footer(text=f"zase meškám | {self.bot.user.name}")
+        embed.set_footer(text=f"I'm late again | {self.bot.user.name}")
         embed.set_author(name=self.bot.user.name, icon_url=self.bot.user.avatar_url)
 
         await ctx.send(embed=embed)
